b47394ce3c4c70dabc18d3a0583bf0dcb8f1a5df
MasahiroToyooka <ehara2481@gmail.com>
2019-08-06 02:53:40 +0900

    Initial Commit

---
+++ UserLIst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLIst
+//
+//  Created by 豊岡正紘 on 2019/08/06.
+//  Copyright © 2019 Masahiro Toyooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
