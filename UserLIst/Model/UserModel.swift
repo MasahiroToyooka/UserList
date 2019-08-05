d2c7f5f1556bcf61b7347afa68cd1ce9f5d13e63
MasahiroToyooka <ehara2481@gmail.com>
2019-08-06 03:10:30 +0900

    Modelファイルの作成

---
+++ UserLIst/Model/UserModel.swift
@@ -0,0 +1,20 @@
+//
+//  UserModel.swift
+//  UserLIst
+//
+//  Created by 豊岡正紘 on 2019/08/06.
+//  Copyright © 2019 Masahiro Toyooka. All rights reserved.
+//
+
+import Foundation
+
+struct UserModel {
+    
+    let name: String
+    let hobby: String
+    
+    init(name: String, hobby: String) {
+        self.name = name
+        self.hobby = hobby
+    }
+}
