92bce897892c41ff923c7635795cb16b51e9b084
MasahiroToyooka <ehara2481@gmail.com>
2019-08-06 18:46:05 +0900

    ラベルに名前を追加

--- UserLIst/ViewController.swift
+++ UserLIst/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    var members: [UserModel] = [
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>),
+//        UserModel(name: <#T##String#>, hobby: <#T##String#>)
+//    ]
+//    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
