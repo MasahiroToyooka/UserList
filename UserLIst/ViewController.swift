4cd5176ed501916eb0830182b5e6efe82f22f2bf
MasahiroToyooka <ehara2481@gmail.com>
2019-08-07 22:24:05 +0900

    モデルの内容を追加

--- UserLIst/ViewController.swift
+++ UserLIst/ViewController.swift
@@ -10,24 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var members: [UserModel] = [
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>),
-        UserModel(name: <#T##String#>, hobby: <#T##String#>)
-    ]
-    
-    
-    
+
+    @IBOutlet var buttonCollection: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupButton()
     }
-
-
+    
+    // segue遷移前動作
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "showDetail", let vc = segue.destination as? NextViewController else { return }
+        
+//        vc.dataNum = buttonCollection[1].tag
+    }
+    
+    
+    func setupButton() {
+        for i in 0..<buttonCollection.count {
+            let button = buttonCollection[i]
+            
+            button.addTarget(self, action: #selector(pushAction(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc func pushAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "showDetail", sender: nil)
+        print(sender.tag)
+    }
 }
 
